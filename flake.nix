281ef8d7fae558a225b666275f34472ca07c743a
Knarkzel <knarkzel@gmail.com>
2023-03-14 19:58:11 +0100

    Clear background

--- flake.nix
+++ flake.nix
@@ -16,11 +16,23 @@
   }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs {inherit system;};
+      packages = [
+        pkgs.cmake
+        pkgs.pkg-config
+        pkgs.fontconfig
+        pkgs.xorg.libX11
+        pkgs.xorg.libXcursor
+        pkgs.xorg.libXrandr
+        pkgs.xorg.libXi
+        pkgs.vulkan-tools
+        pkgs.vulkan-headers
+        pkgs.vulkan-validation-layers
+        pkgs.vulkan-loader
+      ];
     in {
       devShell = pkgs.mkShell {
-        buildInputs = [
-          pkgs.cmake
-        ];
+        buildInputs = packages;
+        shellHook = ''export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.lib.makeLibraryPath packages}"'';
       };
     });
 }
